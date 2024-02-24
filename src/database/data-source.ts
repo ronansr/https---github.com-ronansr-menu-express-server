66f12230c96ea1d3d94e98ddbb6c0e58a0ff70a7
ronan <ronansr@live.com>
2024-02-24 02:57:18 -0300

    testando bd

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -6,11 +6,12 @@ const PORT = process.env.TYPEORM_PORT as unknown as number | undefined;
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: process.env.TYPEORM_HOST,
-  port: PORT,
-  username: process.env.TYPEORM_USERNAME,
-  password: process.env.TYPEORM_PASSWORD,
-  database: "test_orm",
+  url: "postgres://postgres.plsfjzwayumzwtqprbwr:kjZRW90rhphgWe6y@aws-0-sa-east-1.pooler.supabase.com:5432/postgres", //process.env.TYPEORM_HOST,
+  port: 5432, //PORT,
+  username: "postgres", //process.env.TYPEORM_USERNAME,
+  password: "kjZRW90rhphgWe6y", //process.env.TYPEORM_PASSWORD,
+  database: "postgres", //"test_orm",
   entities: [`${__dirname}/**/entities/*.{ts, js}`],
   migrations: [`${__dirname}/**/migrations/*.{ts, js}`],
+  ssl: { rejectUnauthorized: false },
 });
